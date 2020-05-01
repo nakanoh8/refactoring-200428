5d7f9d824d96599be03a816093277148928652e0
nakanoh <murry.xxx.7@gmail.com>
2020-05-01 18:25:30 +0900

    p9 amountFor()の戻り値をresultに変更

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
     let thisAmount = amountFor(perf, play);
-    
+
     // ボリューム特典のポイントを加算
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 喜劇のときは 10人につき、 さらにポイントを加算
@@ -38,23 +38,23 @@ function statement(invoice, plays) {
 }
 
 function amountFor(perf, play) {
-  let thisAmount = 0;
+  let result = 0;
   switch (play.type) {
     case "tragedy":
-      thisAmount = 40000;
+        result = 40000;
       if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
+        result += 1000 * (perf.audience - 30);
       }
       break;
     case "comedy":
-      thisAmount = 30000;
+        result = 30000;
       if (perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
+        result += 10000 + 500 * (perf.audience - 20);
       }
-      thisAmount += 300 * perf.audience;
+      result += 300 * perf.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
   }
-  return thisAmount;
+  return result;
 }
