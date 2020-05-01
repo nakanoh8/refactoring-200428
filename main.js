23ed56b4d01fea10e930c50e61d8d060ab5775ca
nakanoh <murry.xxx.7@gmail.com>
2020-05-01 21:24:22 +0900

    p17 関数の抽出（format()）

--- main.js
+++ main.js
@@ -13,12 +13,6 @@ function statement(invoice, plays) {
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
-  const format = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2,
-  }).format;
-
   for (let perf of invoice.performances) {
 
     volumeCredits += volumeCreditsFor(perf);
@@ -69,4 +63,12 @@ function volumeCreditsFor(aPerformance){
     // 喜劇のときは 10人につき、 さらにポイントを加算
     if ("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
     return result;
+}
+
+function format(aNumber){
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2,
+      }).format(aNumber);
 }
