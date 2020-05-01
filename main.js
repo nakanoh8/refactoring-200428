1a929d1e98fc0818c837b0772450514ff2633cf0
nakanoh <murry.xxx.7@gmail.com>
2020-05-01 19:18:27 +0900

    p10 amountFor()の引数perfをaPerformanceに変更

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ console.log(statement(invoices[0], plays));
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
-  let result = "Statement for ${invoice.customer}¥n";
+  let result = "Statement for ${invoice.customer}\n";
 
   const format = new Intl.NumberFormat("en-US", {
     style: "currency",
@@ -37,21 +37,21 @@ function statement(invoice, plays) {
   return result;
 }
 
-function amountFor(perf, play) {
+function amountFor(aPerformance, play) {
   let result = 0;
   switch (play.type) {
     case "tragedy":
         result = 40000;
-      if (perf.audience > 30) {
-        result += 1000 * (perf.audience - 30);
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
       }
       break;
     case "comedy":
         result = 30000;
-      if (perf.audience > 20) {
-        result += 10000 + 500 * (perf.audience - 20);
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
       }
-      result += 300 * perf.audience;
+      result += 300 * aPerformance.audience;
       break;
     default:
       throw new Error(`unknown type: ${play.type}`);
