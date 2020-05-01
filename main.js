55652ef03cd759e0f9b91be09dd909e32544f348
nakanoh <murry.xxx.7@gmail.com>
2020-05-01 21:28:33 +0900

    p17 関数宣言の変更（format() > usd()）

--- main.js
+++ main.js
@@ -18,13 +18,13 @@ function statement(invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // 注文の内訳を出力
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
 
   return result;
@@ -65,10 +65,10 @@ function volumeCreditsFor(aPerformance){
     return result;
 }
 
-function format(aNumber){
+function usd(aNumber){
     return new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD',
         minimumFractionDigits: 2,
-      }).format(aNumber);
+      }).format(aNumber / 100);
 }
