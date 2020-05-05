e29b6eda02de393b13e999cbbbbc07b352f10240
nakanoh <murry.xxx.7@gmail.com>
2020-05-05 10:09:35 +0900

    p29-2

--- main.js
+++ main.js
@@ -11,6 +11,8 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
@@ -56,6 +58,22 @@ function statement(invoice, plays) {
       result += Math.floor(aPerformance.audience / 5);
     return result;
   }
+
+  function totalVolumeCredits(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+        result += perf.volumeCredits;
+    }
+    return result;
+  }
+
+  function totalAmount(data){
+      let result = 0;
+    for (let perf of data.performances) {
+        result += perf.amount;
+    }
+    return result;
+  }
 }
 
 function renderPlainText(data, plays) {
@@ -68,8 +86,8 @@ function renderPlainText(data, plays) {
     } seats)\n`;
   }
 
-  result += `Amount owed is ${usd(totalAmount())}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
   
 
@@ -79,21 +97,5 @@ function renderPlainText(data, plays) {
       currency: "USD",
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
-  }
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of data.performances) {
-        result += perf.volumeCredits;
-    }
-    return result;
-  }
-
-  function totalAmount(){
-      let result = 0;
-    for (let perf of data.performances) {
-        result += perf.amount;
-    }
-    return result;
   }
 }
