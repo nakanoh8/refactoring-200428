5c2716a9587ffd04bfa898a86f99f58d94c00217
nakanoh <murry.xxx.7@gmail.com>
2020-05-08 15:56:30 +0900

    p12-3 amountFor()のplayパラメータ削除 : test/statement > green

--- src/chapter1/main.js
+++ src/chapter1/main.js
@@ -19,7 +19,7 @@ export function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf, playFor(perf));
+        let thisAmount = amountFor(perf);
 
         // ボリューム特典のポイントを加算
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -37,7 +37,7 @@ export function statement(invoice, plays) {
 
     return result;
 
-    function amountFor(aPerformances, play){
+    function amountFor(aPerformances){
         let result = 0;
         switch (playFor(aPerformances).type) {
             case "tragedy":
