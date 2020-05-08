6a30cff7180455c3fc63cb76d00d8cfe0b91449a
nakanoh <murry.xxx.7@gmail.com>
2020-05-08 15:02:44 +0900

    p3 スタート地点 >テスト追加 >成功

--- test/chapter1/main_test.js
+++ test/chapter1/main_test.js
@@ -10,6 +10,15 @@ describe("statement", function () {
     const require = createRequire(import.meta.url);
     let invoices = require("../../data/invoices.json");
     let plays = require("../../data/plays.json");
-    expect(statement(invoices[0], plays)).equal(5);
+
+    const output =
+      "Statement for BigCo\n" +
+      " Hamlet: $650.00 (55 seats)\n" +
+      " As You Like It: $580.00 (35 seats)\n" +
+      " Othello: $500.00 (40 seats)\n" +
+      "Amount owed is $1,730.00\n" +
+      "You earned 47 credits\n";
+
+    expect(statement(invoices[0], plays)).equal(output);
   });
 });
